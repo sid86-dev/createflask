06453d1035a3c1d48ffbed15493f63c562290078
sid86-dev <sid86harth@gmail.com>
2021-09-21 12:28:52 +0530

    dir defalut name

--- build.bat
+++ build.bat
@@ -1,10 +1,9 @@
 @echo off
 
 color a
-set /p f_name=Enter folder name:
 
-mkdir  %f_name%
-cd %f_name%
+mkdir  flaskapp
+cd flaskapp
 mkdir static
 cd static
 mkdir css
@@ -19,15 +18,15 @@ set LF=^
 
 
 rem Two empty lines are required
-set data_flask=from flask import Flask, render_template!LF!app=Flask(__name__)!LF!@app.route('/')!LF!def index():!LF!     return render_template('index.html')!LF!if __name__=='__main__':!LF!    app.run(port=4000)
-set data_index=Hello world!LF!this is %f_name% flask app
+set data_flask=from flask import Flask, render_template!LF!app=Flask(__name__)!LF!@app.route('/')!LF!def index():!LF!     return render_template('index.html')!LF!if __name__=='__main__':!LF!    app.run(debug=True)
+set data_index=Hello world!LF!this is flask app
 set data_req=click==8.0.1!LF!Flask==2.0.1!LF!importlib-metadata==4.8.1!LF!itsdangerous==2.0.1!LF!Jinja2==3.0.1!LF!MarkupSafe==2.0.1!LF!typing-extensions==3.10.0.2!LF!Werkzeug==2.0.1!LF!zipp==3.5.0!LF!colorama==0.4.4
 echo !data_index! > "index.html"
 cd ..
 echo !data_flask! > "app.py"
 echo !data_req! > "requirements.txt"
 @REM pause
-start chrome http://127.0.0.1:4000/
+start chrome http://127.0.0.1:5000/
 python app.py
 clear
 
